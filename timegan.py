45b260a936073d184d27634c86ab430da94b6d50
Fredrik <fre-svee@hotmail.no>
2024-03-13 12:37:33 +0100

    save 3 frames to inspect

--- timegan.py
+++ timegan.py
@@ -300,8 +300,13 @@ def timegan (ori_data, parameters, reproduce=False):
 
   x = datetime.datetime.now()
   timestamp = x.strftime("%d_%m_%y__%Hh%M")
-  gen_data_df = pd.DataFrame(generated_data_curr)
-  gen_data_df.to_csv(f'generated_dataframes/frames_{timestamp}.csv')
+  gen_data_df = pd.DataFrame(generated_data_curr[0])
+  gen_data_df1 = pd.DataFrame(generated_data_curr[1])
+  gen_data_df2 = pd.DataFrame(generated_data_curr[2])
+
+  gen_data_df.to_csv(f'generated_dataframes/frame0_{timestamp}.csv')
+  gen_data_df1.to_csv(f'generated_dataframes/frame1_{timestamp}.csv')
+  gen_data_df2.to_csv(f'generated_dataframes/frame2_{timestamp}.csv')
 
   ## Save the trained model
   saver.save(sess, 'trained_models/model1')  
